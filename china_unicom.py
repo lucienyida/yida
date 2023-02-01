4cf78d97104b70281f3582095a2c07c564d4a4d0
lucienyida <96853173+lucienyida@users.noreply.github.com>
2023-02-01 11:08:49 +0800

    中国联通

--- china_unicom.py
+++ china_unicom.py
@@ -10,14 +10,14 @@
 联通app抽奖 入口:联通app 搜索 阅读专区 进入话费派送中
 1. 脚本仅供学习交流使用, 请在下载后24h内删除
 2. 需要第三方库 pycryptodome 支持 命令行安装 pip3 install pycryptodome或者根据自己环境自行安装
-3. 环境变量说明 PHONE_NUM(必需) UNICOM_LOTTER(选填) UNICOM_USERAGENT(选填) 自行新建环境变量添加
-    PHONE_NUM 为你的手机号
+3. 环境变量说明 PHONE_NUM(必需) UNICOM_LOTTER(选填) 自行新建环境变量添加
+    PHONE_NUM  格式：手机号#UA     多账号使用&隔开    UA(选填) 为联通app的useragent 随便一个数据包的请求头里应该都有 建议自己抓一个填上 不填也能跑 数据内容参考 line 49 双引号的内容
+    UA例子：Mozilla/5.0 (iPhone; CPU iPhone OS 15_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 unicom{version:iphone_c@10.0001}
     UNICOM_LOTTER 默认自动抽奖, 若不需要 则添加环境变量值为 False
-    UNICOM_USERAGENT 联通app的useragent 随便一个数据包的请求头里应该都有 建议自己抓一个填上 不填也能跑 数据内容参考 line 44 双引号的内容
-    推送通知的变量同青龙 只写了tgbot(支持反代api)和pushplus
 """
-"""updateTime: 2023.1.1  log: 更新aes加密key
-updateTime: 2022.12.1  log: 活动重新上架 改用 pycryptodome 替代 cryptography 进行aes加密
+"""
+updateTime: 2023.1.1  log: 更新aes加密key
+updateTime: 2022.12.1 log: 活动重新上架 改用 pycryptodome 替代 cryptography 进行aes加密
 updateTime: 2022.9.1  log: 每个月活动id改变更新
 """
 
@@ -30,19 +30,28 @@ from os import environ
 from sys import stdout, exit
 from base64 import b64encode
 from json import dumps
-
+from tools.notify import send
 from tools.encrypt_symmetric import Crypt
 from tools.send_msg import push
 from tools.tool import get_environ, random_sleep
-# random_sleep(0, 1600)
+#random_sleep(0, 1600)
+from tools.ql_api import get_cookie
+import threading
 
+
+
+msg_str = "联通话费兑换路径：联通APP搜索阅读--->阅读专区--->我的--->话费红包，可兑换3元或者5元话费抵扣券，最后使用沃钱包支付即可\n\n"
 
 """主类"""
 class China_Unicom:
-    def __init__(self, phone_num):
+    def __init__(self, phone_num, run_ua):
         self.phone_num = phone_num
         default_ua = f"Mozilla/5.0 (Linux; Android {randint(8, 13)}; SM-S908U Build/TP1A.220810.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{randint(95, 108)}.0.5359.128 Mobile Safari/537.36; unicom{{version:android@9.0{randint(0,6)}00,desmobile:{self.phone_num}}};devicetype{{deviceBrand:,deviceModel:}};{{yw_code:}}"
-        run_ua = get_environ(key="UNICOM_USERAGENT", default=default_ua, output=False)
+        #run_ua = get_environ(key="UNICOM_USERAGENT", default=default_ua, output=False)
+        if run_ua is None or run_ua == "":
+            run_ua = default_ua
+        # print("使用的UA："+run_ua)
+
         self.headers = {
             "Host": "10010.woread.com.cn",
             "Accept": "application/json, text/plain, */*",
@@ -67,13 +76,22 @@ class China_Unicom:
         m = md5Encode(str.encode(encoding='utf-8'))
         return m.hexdigest()
 
-    def req(self, url, crypt_text):
-        body = {
-            "sign": b64encode(Crypt(crypt_type="AES", key="update!@#1234567", iv="16-Bytes--String", mode="CBC").encrypt(crypt_text).encode()).decode()
-        }
-        self.headers["Content-Length"] = str(len(dumps(body).replace(" ", "")))
-        data = post(url, headers=self.headers, json=body).json()
-        return data
+    def req(self, url, crypt_text, retry_num=5):
+        while retry_num > 0:
+            body = {
+                "sign": b64encode(Crypt(crypt_type="AES", key="update!@#1234567", iv="16-Bytes--String", mode="CBC").encrypt(crypt_text).encode()).decode()
+            }
+            self.headers["Content-Length"] = str(len(dumps(body).replace(" ", "")))
+            try:
+                res = post(url, headers=self.headers, json=body)
+                data = res.json()
+                return data
+            except Exception as e:
+                print(f"本次请求失败, 正在重新发送请求 剩余次数{retry_num}")
+                print(f"本次请求失败原因------{e}")
+                retry_num -= 1
+                sleep(5)
+                return self.req(url, crypt_text, retry_num)
 
     def referer_login(self):
         date = datetime.today().__format__("%Y%m%d%H%M%S")
@@ -103,6 +121,7 @@ class China_Unicom:
             exit(0)
 
     def watch_video(self):
+        global msg_str #声明我们在函数内部使用的是在函数外部定义的全局变量msg_str
         self.print_now("看广告获取积分任务: ")
         url = "https://10010.woread.com.cn/ng_woread_service/rest/activity/yearEnd/obtainScoreByAd"
         date = datetime.today().__format__("%Y%m%d%H%M%S")
@@ -112,7 +131,8 @@ class China_Unicom:
             self.print_now(data)
             if self.fail_num == 3:
                 self.print_now("当前任务出现异常 且错误次数达到3次 请手动检查")
-                push("某通阅读", "当前任务出现异常 且错误次数达到3次 请手动检查")
+                # push("某通阅读", "当前任务出现异常 且错误次数达到3次 请手动检查")
+                msg_str += f"账号{self.phone_num}当前任务出现异常 且错误次数达到3次 请手动检查\n\n"
                 exit(0)
             if data["code"] == "9999":
                 self.print_now("当前任务出现异常 正在重新执行")
@@ -123,11 +143,12 @@ class China_Unicom:
         return True
 
     def read_novel(self):
+        global msg_str #声明我们在函数内部使用的是在函数外部定义的全局变量msg_str
         self.get_cardid()
         self.get_cntindex()
         self.get_chapterallindex()
-        self.print_now("正在执行观看150次小说, 此过程较久, 最大时长为150 * 8s = 20min")
-        for i in range(150):
+        self.print_now(f"你的账号{self.phone_num} ：正在执行观看300次小说, 此过程较久, 最大时长为300 * 8s = 40min")
+        for i in range(300):
             date = datetime.today().__format__("%Y%m%d%H%M%S")
             chapterAllIndex = choice(self.chapterallindex_list)
             url = f"https://10010.woread.com.cn/ng_woread_service/rest/cnt/wordsDetail?catid={self.catid}&pageIndex={self.pageIndex}&cardid={randint(10000, 99999)}&cntindex={self.cntindex}&chapterallindex={chapterAllIndex}&chapterseno=3"
@@ -135,13 +156,14 @@ class China_Unicom:
             data = self.req(url, crypt_text)
             if self.fail_num == 3:
                 self.print_now("当前任务出现异常 且错误次数达到3次 请手动检查")
-                push("某通阅读", "阅读任务出现异常 且错误次数达到3次 请手动检查")
+                # push("某通阅读", "阅读任务出现异常 且错误次数达到3次 请手动检查")
+                msg_str += f"账号{self.phone_num}阅读任务出现异常 且错误次数达到3次 请手动检查\n\n"
                 exit(0)
             if data.get("code") != "0000":
                 self.print_now("阅读小说发生异常, 正在重新登录执行, 接口返回")
                 self.print_now(data)
                 return self.main()
-            sleep(uniform(2, 8))
+            sleep(uniform(2, 4))
 
     def query_score(self):
         url = "https://10010.woread.com.cn/ng_woread_service/rest/activity/yearEnd/queryUserScore"
@@ -150,7 +172,7 @@ class China_Unicom:
         data = self.req(url, crypt_text)
         total_score = data["data"]["validScore"]
         self.lotter_num = int(total_score / 50)
-        self.print_now(f"你的账号当前有积分{total_score}, 可以抽奖{self.lotter_num}次")
+        self.print_now(f"你的账号{self.phone_num}当前有积分{total_score}, 可以抽奖{self.lotter_num}次")
 
     def get_activetion_id(self):
         url = "https://10010.woread.com.cn/ng_woread_service/rest/activity/yearEnd/queryActiveInfo"
@@ -221,6 +243,7 @@ class China_Unicom:
         print(data)
 
     def query_red(self):
+        global msg_str #声明我们在函数内部使用的是在函数外部定义的全局变量msg_str
         url = "https://10010.woread.com.cn/ng_woread_service/rest/phone/vouchers/queryTicketAccount"
         date = datetime.today().__format__("%Y%m%d%H%M%S")
         crypt_text = f'{{"timestamp":"{date}","token":"{self.userinfo["token"]}","userId":"{self.userinfo["userid"]}","userIndex":{self.userinfo["userindex"]},"userAccount":"{self.userinfo["phone"]}","verifyCode":"{self.userinfo["verifycode"]}"}}'
@@ -228,11 +251,13 @@ class China_Unicom:
         if data["code"] == "0000":
             can_use_red = data["data"]["usableNum"] / 100
             if can_use_red >= 3:
-                self.print_now(f"查询成功 你当前有话费红包{can_use_red} 可以去兑换了")
-                push("某通阅读", f"查询成功 你当前有话费红包{can_use_red} 可以去兑换了")
+                self.print_now(f"账号{self.phone_num}查询成功 你当前有话费红包{can_use_red} 可以去兑换了")
+                # push("某通阅读", f"账号{self.phone_num}查询成功 你当前有话费红包{can_use_red} 可以去兑换了")
+                msg_str += f"账号{self.phone_num}查询成功 你当前有话费红包{can_use_red} 可以去兑换了\n\n"
             else:
-                self.print_now(f"查询成功 你当前有话费红包{can_use_red} 不足兑换的最低额度")
-                push("某通阅读", f"查询成功 你当前有话费红包{can_use_red} 不足兑换的最低额度")
+                self.print_now(f"账号{self.phone_num}查询成功 你当前有话费红包{can_use_red} 不足兑换的最低额度")
+                # push("某通阅读", f"账号{self.phone_num}查询成功 你当前有话费红包{can_use_red} 不足兑换的最低额度")
+                msg_str += f"账号{self.phone_num}查询成功 你当前有话费红包{can_use_red} 不足兑换的最低额度\n\n"
 
     def main(self):
         self.referer_login()
@@ -251,10 +276,54 @@ class China_Unicom:
         self.query_red()
 
 
-if __name__ == "__main__":
-    """读取环境变量"""
-    phone_num = get_environ("PHONE_NUM")
-    unicom_lotter = get_environ("UNICOM_LOTTER", default=True)
-    if phone_num == "":
+        
+def start(phone,run_ua):
+    if phone == "":
         exit(0)
-    China_Unicom(phone_num).main()
+    China_Unicom(phone,run_ua).main()
+    print("\n")
+    print("\n")        
+
+    
+    
+        
+if __name__ == "__main__":
+    #unicom_lotter = get_environ("UNICOM_LOTTER", default=True)
+    unicom_lotter = get_cookie("UNICOM_LOTTER", True, False)
+    """读取环境变量"""
+    l = []
+    user_map = []
+    cklist = get_cookie("PHONE_NUM")
+    for i in range(len(cklist)):
+        #以#分割开的ck
+        split1 = cklist[i].split("&")
+        if len(split1)>1:
+            for j in range(len(split1)):
+                user_map.append(split1[j])
+        else:
+            user_map.append(cklist[i])
+
+    
+    
+    for i in range(len(user_map)):
+        phone=""
+        info = user_map[i].split("&")[0]
+        #以#分割开的ck
+        split1 = info.split("#")
+        run_ua = ""
+        phone = split1[0]
+        if len(split1)>1:
+            run_ua = split1[1] + f";devicetype{{deviceBrand:,deviceModel:}};{{yw_code:}}"
+
+        print('开始执行第{}个账号：{}'.format((i+1),phone))
+        if phone == "":
+            print("当前账号未填写手机号 跳过")
+            print("\n")
+            continue
+        p = threading.Thread(target=start,args=(phone, run_ua))
+        l.append(p)
+        p.start()
+        print("\n")
+    for i in l:
+        i.join()
+    send("联通阅读",msg_str)
